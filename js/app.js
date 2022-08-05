7f3b9f9cfeb3214b2354a0f36d279cd97736d1ce
labush <aestdata@gmail.com>
2022-08-05 10:10:23 +0200

    1 - Initialisation du projet

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const App = {
+    data(){
+        return {
+            titre_accueil: 'Accueil',
+            titre_gestion: 'Panneau de gestion'
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
