c666d01df6a1d62a7508eb97ca898f7fb34b1c18
labush <aestdata@gmail.com>
2022-08-05 14:05:52 +0200

    2 - Rendu conditionnel et navigation

--- js/app.js
+++ js/app.js
@@ -2,7 +2,45 @@ const App = {
     data(){
         return {
             titre_accueil: 'Accueil',
-            titre_gestion: 'Panneau de gestion'
+            titre_gestion: 'Panneau de gestion',
+            showAccueil: false,
+            showCreateForm: false,
+            showStudentsList: false,
+        }
+    },
+    mounted() {
+        this.changeNavigationState("accueil")
+    },
+    methods: {
+        goToHome() {
+            this.changeNavigationState("accueil")
+        },
+        goToCreateForm() {
+            this.changeNavigationState("create")
+        },
+        goToStudentsList() {
+            this.changeNavigationState("list")
+        },
+        changeNavigationState(destination) {
+            this.showAccueil = false
+            this.showCreateForm = false
+            this.showStudentsList = false
+
+            switch (destination) {
+                case "accueil":
+                    this.showAccueil = true
+                    break;
+                case "create":
+                    this.showCreateForm = true
+                    break;
+                case "list":
+                    this.showStudentsList = true
+                    break;
+            
+                default:
+                    this.showAccueil = true
+                    break;
+            }
         }
     }
 }
